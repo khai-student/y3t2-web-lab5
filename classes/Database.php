1c710d39fd62304f7a7f67bdf75049f4cfb063f3
Юрий Власов < >
2017-05-03 09:05:51 +0300

    Sidebar request sections successl

---
+++ classes/Database.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 11.04.2017
+ * Time: 20:50
+ */
+class Database
+{
+
+}
---
+++ classes/Debugger.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 11.04.2017
+ * Time: 21:01
+ */
+class Debugger
+{
+
+}
